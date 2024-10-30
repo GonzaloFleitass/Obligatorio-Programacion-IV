2615b1ab610450072edd3643b7c6373941b48a74
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2024-10-30 10:02:07 -0300

    inicio

---
+++ Obligatorio Prog IV/Obligatorio Prog IV/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 30/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
