c39abfe3f9c90befd9d2c8f50956f5b9ad98f3aa
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2024-11-12 19:07:01 -0300

    hola

---
+++ Obligatorio Prog IV/Obligatorio Prog IV/Camioneros.cpp
@@ -0,0 +1,8 @@
+//
+//  Camioneros.cpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 12/11/24.
+//
+
+#include "Camioneros.hpp"
---
+++ Obligatorio Prog IV/Obligatorio Prog IV/Camioneros.hpp
@@ -0,0 +1,13 @@
+//
+//  Camioneros.hpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 12/11/24.
+//
+
+#ifndef Camioneros_hpp
+#define Camioneros_hpp
+
+#include <stdio.h>
+
+#endif /* Camioneros_hpp */
