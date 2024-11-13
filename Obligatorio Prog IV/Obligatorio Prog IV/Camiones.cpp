ed7322ea7746f3c94079e86790dc8c25dc7ff570
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2024-11-13 09:46:53 -0300

    complementos

---
+++ Obligatorio Prog IV/Obligatorio Prog IV/Camiones.cpp
@@ -0,0 +1,8 @@
+//
+//  Camiones.cpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 13/11/24.
+//
+
+#include "Camiones.hpp"
---
+++ Obligatorio Prog IV/Obligatorio Prog IV/Camiones.hpp
@@ -0,0 +1,13 @@
+//
+//  Camiones.hpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 13/11/24.
+//
+
+#ifndef Camiones_hpp
+#define Camiones_hpp
+
+#include <stdio.h>
+
+#endif /* Camiones_hpp */
