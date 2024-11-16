f6565cdede3e2cf8ab76e8126e8a4b2c24a2f773
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2024-11-16 16:24:04 -0300

    actualizacion

---
+++ Obligatorio Prog IV/Obligatorio Prog IV/Fachada.cpp
@@ -0,0 +1,8 @@
+//
+//  Fachada.cpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 16/11/24.
+//
+
+#include "Fachada.hpp"
---
+++ Obligatorio Prog IV/Obligatorio Prog IV/Fachada.hpp
@@ -0,0 +1,13 @@
+//
+//  Fachada.hpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 16/11/24.
+//
+
+#ifndef Fachada_hpp
+#define Fachada_hpp
+
+#include <stdio.h>
+
+#endif /* Fachada_hpp */
