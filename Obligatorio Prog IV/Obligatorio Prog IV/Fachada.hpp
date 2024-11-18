7613996e93898190a26a8c76cd579dea035ab5aa
GonzaloFleitass <144866796+GonzaloFleitass@users.noreply.github.com>
2024-11-18 09:52:18 -0300

    Update Fachada.hpp

--- Obligatorio Prog IV/Obligatorio Prog IV/Fachada.hpp
+++ Obligatorio Prog IV/Obligatorio Prog IV/Fachada.hpp
@@ -20,7 +20,7 @@ private:
 public:
     CapaLogica();
     void Insert();
-    void Listadodetalladocamion();
+    void Listadodetalladocamion(int ci);
     void nuevocamionero();
     void Listarcamionerosregistrados();
     int Cantidadmetroscubicos();
