ee58a3fc2801439f0356cde42363ec7363279f5d
GonzaloFleitass <144866796+GonzaloFleitass@users.noreply.github.com>
2024-11-18 12:49:47 -0300

    Update Fachada.cpp

--- Obligatorio Prog IV/Obligatorio Prog IV/Fachada.cpp
+++ Obligatorio Prog IV/Obligatorio Prog IV/Fachada.cpp
@@ -102,6 +102,6 @@ void CapaLogica:: listadoCamionesRegistrados(){
 int CapaLogica::Cantidadmetroscubicos(){
   return camiones.Cantidadmetroscubicos();
 }
-void cantCamionesCadaTipo(int G, int S, int C){
+void CantidadCamionesCadaTipo(int G, int S, int C){
     camiones.cantCamionesCadaTipo(G,S,C);
 }
