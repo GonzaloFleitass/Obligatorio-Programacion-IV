3a02ed4e4f0b9a704d56bcdc5f53bef67b17f0e1
Miguel <miguelescanocedres@icloud.com>
2024-11-19 19:49:59 -0300

    fesf

--- Obligatorio Prog IV/Obligatorio Prog IV/main.cpp
+++ Obligatorio Prog IV/Obligatorio Prog IV/main.cpp
@@ -49,7 +49,7 @@ int main() {
                 break;
             }
             case 2:
-                //logica.ListadoCamiones();
+                logica.ListadoCamiones();
                 break;
             case 3:{
                 String mat,nombre;
