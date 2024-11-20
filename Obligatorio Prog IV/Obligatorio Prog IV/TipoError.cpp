f460a301dc88a850434d81f61c996c9d4ebf1e9a
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2024-11-20 10:06:20 -0300

    modulo error

---
+++ Obligatorio Prog IV/Obligatorio Prog IV/TipoError.cpp
@@ -0,0 +1,8 @@
+//
+//  TipoError.cpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 19/11/24.
+//
+
+#include "TipoError.hpp"
---
+++ Obligatorio Prog IV/Obligatorio Prog IV/TipoError.hpp
@@ -0,0 +1,13 @@
+//
+//  TipoError.hpp
+//  Obligatorio Prog IV
+//
+//  Created by Gonzalo Fleitas on 19/11/24.
+//
+
+#ifndef TipoError_hpp
+#define TipoError_hpp
+
+#include <stdio.h>
+
+#endif /* TipoError_hpp */
